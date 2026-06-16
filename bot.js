c92a27fb7c633e8c11a3d88d48f519c8531cf858
Mintlifly <vishnugrowbizsolutions@gmail.com>
2026-06-16 16:16:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,19 @@
+<script type='text/javascript'>
+	function initEmbeddedMessaging() {
+		try {
+			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
 
+			embeddedservice_bootstrap.init(
+				'00DWC00000CLPyz',
+				'GoGuardian_Support',
+				'https://goguardian--uat.sandbox.my.site.com/ESWGoGuardianSupport1770722306967',
+				{
+					scrt2URL: 'https://goguardian--uat.sandbox.my.salesforce-scrt.com'
+				}
+			);
+		} catch (err) {
+			console.error('Error loading Embedded Messaging: ', err);
+		}
+	};
+</script>
+<script type='text/javascript' src='https://goguardian--uat.sandbox.my.site.com/ESWGoGuardianSupport1770722306967/assets/js/bootstrap.min.js' onload='initEmbeddedMessaging()'></script>
