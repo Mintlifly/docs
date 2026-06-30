c64fc43f071583e02b30fc1e168d2fe4bb838086
Mintlifly <vishnugrowbizsolutions@gmail.com>
2026-06-30 17:04:09 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
-<script type='text/javascript'>
-	function initEmbeddedMessaging() {
-		try {
-			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+function initEmbeddedMessaging() {
+  try {
+    embeddedservice_bootstrap.settings.language = 'en_US';
+    embeddedservice_bootstrap.init(
+      '00DWC00000CLPyz',
+      'GoGuardian_Support',
+      'https://goguardian--uat.sandbox.my.site.com/ESWGoGuardianSupport1770722306967',
+      { scrt2URL: 'https://goguardian--uat.sandbox.my.salesforce-scrt.com' }
+    );
+  } catch (err) {
+    console.error('Error loading Embedded Messaging: ', err);
+  }
+}
 
-			embeddedservice_bootstrap.init(
-				'00DWC00000CLPyz',
-				'GoGuardian_Support',
-				'https://goguardian--uat.sandbox.my.site.com/ESWGoGuardianSupport1770722306967',
-				{
-					scrt2URL: 'https://goguardian--uat.sandbox.my.salesforce-scrt.com'
-				}
-			);
-		} catch (err) {
-			console.error('Error loading Embedded Messaging: ', err);
-		}
-	};
-</script>
-<script type='text/javascript' src='https://goguardian--uat.sandbox.my.site.com/ESWGoGuardianSupport1770722306967/assets/js/bootstrap.min.js' onload='initEmbeddedMessaging()'></script>
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://goguardian--uat.sandbox.my.site.com/ESWGoGuardianSupport1770722306967/assets/js/bootstrap.min.js';
+script.onload = initEmbeddedMessaging;
+document.body.appendChild(script);
